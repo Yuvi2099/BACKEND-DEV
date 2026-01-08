e3c641fcb9cc80dff3e6710282fa7d8be6d98065
Yuvraj <yuvraj.singh0317@gmail.com>
2026-01-08 10:07:04 +0530

    [YUVRAJ]: added the server file for calculator

---
+++ server.js
@@ -0,0 +1,7 @@
+const calc = require('./calculator');
+
+console.log(calc.add(10, 5));        
+console.log(calc.subtract(10, 5));   
+console.log(calc.multiply(10, 5));   
+console.log(calc.divide(10, 5));     
+console.log(calc.power(2, 3));       
