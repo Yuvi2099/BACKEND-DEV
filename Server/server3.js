01e05ec0f66f1b92a34c084c3badd0183a817162
Yuvraj <yuvraj.singh0317@gmail.com>
2026-01-15 19:08:11 +0530

    [YUVRAJ]:added all the server files

---
+++ Server/server3.js
@@ -0,0 +1,27 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1> Home Page </h1>");
+  }
+
+  else if (req.url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1> About Page </h1>")
+  }
+  else if (req.url === "/contact") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1> Contact Page </h1>")
+  }
+  else{
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1> Page not found </h1>")
+  }
+
+});
+
+server.listen(8080, () => {
+  console.log("server is running");
+});
