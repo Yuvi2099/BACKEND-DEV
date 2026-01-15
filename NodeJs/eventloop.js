a05ac5b8aa7ef935af8ac44cf2aee0cd996e5862
Yuvraj <yuvraj.singh0317@gmail.com>
2026-01-15 19:47:55 +0530

    event loop files are added

---
+++ NodeJs/eventloop.js
@@ -0,0 +1,7 @@
+setTimeout(() => console.log("setTimeout"), 0);
+
+setImmediate(() => console.log("setImmediate"));
+
+process.nextTick(() => console.log("nextTick"));
+
+Promise.resolve().then(() => console.log("promise"));
